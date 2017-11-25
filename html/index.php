00d3ecae8dfc2f218080a21f8aa3f7047fa23589
Joshua Moore <joshuamoore235@gmail.com>
2017-11-25 11:09:58 -0700

    changes

--- html/index.php
+++ html/index.php
@@ -2,6 +2,6 @@
 <?php include 'resources/stylesheet.css'; ?>
 </style>
 <?php
-echo file_get_contents("/var/www/content/header.html");
-echo file_get_contents("/var/www/content/content-main.html");
+$page_content = file_get_contents("/var/www/content/header.html") . file_get_contents("/var/www/content/content-main.html");
+echo $page_content;
 ?>
