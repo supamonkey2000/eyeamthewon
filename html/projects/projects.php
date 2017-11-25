3cde89387b83cb9727f8066ed0cd998daef295e2
Joshua Moore <joshuamoore235@gmail.com>
2017-11-25 16:57:49 -0700

    updated files

---
+++ html/projects/projects.php
@@ -0,0 +1,7 @@
+<style>
+<?php include '/var/www/html/resources/stylesheet.css'; ?>
+</style>
+<?php
+$page_content = file_get_contents("/var/www/content/header.html") . file_get_contents("/var/www/content/content-projects.html");
+echo $page_content;
+?>
